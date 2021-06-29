f3ae35967c6c23f0b75e3b03148397808f315627
Michael Czechowski <mail@dailysh.it>
2021-06-29 16:04:44 +0200

    add git commands (2m)

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 git branch -d gh-pages
 git checkout -b gh-pages
 
+
 rm .gitignore
 rm LICENSE
 rm package.json
@@ -11,3 +12,8 @@ rm -rf node_modules
 find . -iname "*README*" -exec rename README.md index.md '{}' \;
 
 echo wcc.dailysh.it > CNAME
+
+git push --set-upstream origin gh-pages
+git commit -am "deploy new version"
+git push
+git checkout main
